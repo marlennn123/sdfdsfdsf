db6d06c5bb6039cd13a550dbf14f94262479ae71
M-Stark <55349494+M-Stark@users.noreply.github.com>
2024-06-27 10:41:55 +0600

    new project

---
+++ mysite/task/views.py
@@ -0,0 +1,13 @@
+from .models import Task
+from rest_framework import viewsets, permissions
+from .serializers import TaskSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['completed']
+    permission_classes = [permissions.IsAuthenticated]
